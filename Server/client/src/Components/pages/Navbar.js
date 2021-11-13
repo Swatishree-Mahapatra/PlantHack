41fae8e6fa2d73188bcd02fee955eae420a17442
slowgeek <kumarritesh14062000@gmail.com>
2021-11-13 19:55:33 +0530

    Login/Signup Backend

--- Server/client/src/Components/pages/Navbar.js
+++ Server/client/src/Components/pages/Navbar.js
@@ -1,8 +1,52 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
-  useEffect(() => {}, []);
+  const userlogged = JSON.parse(localStorage.getItem("loggedUser"));
+  const navigate = useNavigate();
+
+  const NavOption = () => {
+    if (userlogged) {
+      return (
+        <div className={` mr-0`}>
+          <Link
+            to="/forum"
+            className=" hover:bg-gray-700  px-8 py-2 rounded-md  font-medium"
+          >
+            Forum
+          </Link>
+          <Link
+            to="/leader"
+            className=" hover:bg-gray-700  px-8 py-2 rounded-md  font-medium"
+          >
+            Leaderboard
+          </Link>
+        </div>
+      );
+    } else {
+      return (
+        <>
+          <Link
+            to="/login"
+            className=" hover:bg-gray-700  px-8 py-2 rounded-md  font-medium"
+          >
+            Login
+          </Link>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              navigate("/signup");
+            }}
+            className=" bg-pink-300 hover:bg-gray-900 hover:text-white font-bold py-2 px-4 border  rounded`}
+            "
+          >
+            Signup
+          </button>
+        </>
+      );
+    }
+  };
+  useEffect(() => {}, [userlogged]);
 
   return (
     <header className="bg-gray-800 text-white md:sticky top-0 z-10">
@@ -16,17 +60,11 @@ function Navbar() {
           </Link>
         </Link>
 
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          {/* <Link
-            to="#"
-            className=" hover:bg-gray-700  px-3 py-2 rounded-md text-sm font-medium"
-          >
-            Forum
-          </Link> */}
-        </nav>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center"></nav>
 
         <div className={` mr-0`}>
-          <Link
+          <NavOption />
+          {/* <Link
             to="#"
             className=" hover:bg-gray-700  px-8 py-2 rounded-md  font-medium"
           >
@@ -49,7 +87,7 @@ function Navbar() {
             "
           >
             Signup
-          </button>
+          </button> */}
         </div>
       </div>
     </header>
